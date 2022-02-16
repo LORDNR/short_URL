1c5ea9ff50d6742d4473e0ec3bc3af1a14f754fe
Ratchanon Songmeuang <6250110009@psu.ac.th>
2022-02-16 15:59:41 +0700

    add copy URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import axios from "axios";
 
 function App() {
   const [long_url, setURL] = useState("");
   const [short_URL, setShort_URL] = useState("");
+  const [copySuccess, setCopySuccess] = useState("");
+
+  const copyURL = async () => {
+    short_URL.current.select();
+    document.execCommand("copy");
+
+    e.target.focus();
+    setCopySuccess("Copied!");
+  };
 
   const shortURL = async () => {
     const bitlyAPI = "https://api-ssl.bitly.com/v4/shorten";
@@ -33,7 +42,10 @@ function App() {
       <button onClick={shortURL}>ShortURL</button>
       <h1>
         short URL is : <a href="">{short_URL}</a>{" "}
-      </h1>
+      </h1>{" "}
+      <button onClick={() => navigator.clipboard.writeText(short_URL)}>
+        Copy!!
+      </button>
     </div>
   );
 }
